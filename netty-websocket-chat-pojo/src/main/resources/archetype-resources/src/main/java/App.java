44002a1e28dd72e3d93af31c105eef29bdcb5227
范伟 <dtvikey@163.com>
2026-01-16 10:50:43 +0800

    ok

---
+++ netty-websocket-chat-pojo/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.yqrb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
