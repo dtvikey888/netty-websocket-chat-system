2b6a4f1f814d36687076624e2bc5624440623fc4
范伟 <dtvikey@163.com>
2026-02-06 17:22:17 +0800

    售前Netty配置

--- netty-websocket-chat-service/src/main/java/com/yqrb/util/SpringContextUtil.java
+++ netty-websocket-chat-service/src/main/java/com/yqrb/util/SpringContextUtil.java
@@ -6,27 +6,34 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 /**
- * Spring上下文工具类：用于在非Spring容器管理的类中（如Netty Handler）获取Bean
+ * Spring上下文工具类（Netty必备）
  */
 @Component
 public class SpringContextUtil implements ApplicationContextAware {
     private static ApplicationContext applicationContext;
 
     @Override
-    public void setApplicationContext(ApplicationContext context) throws BeansException {
-        SpringContextUtil.applicationContext = context;
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContextUtil.applicationContext = applicationContext;
     }
 
     /**
      * 获取Spring容器中的Bean
-     * @param clazz Bean的类类型
-     * @param <T> 泛型
-     * @return Bean实例
      */
     public static <T> T getBean(Class<T> clazz) {
         if (applicationContext == null) {
-            throw new RuntimeException("Spring上下文未初始化，无法获取Bean");
+            return null;
         }
         return applicationContext.getBean(clazz);
+    }
+
+    /**
+     * 根据名称获取Bean
+     */
+    public static Object getBean(String beanName) {
+        if (applicationContext == null) {
+            return null;
+        }
+        return applicationContext.getBean(beanName);
     }
 }
